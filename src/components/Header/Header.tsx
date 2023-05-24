8d93279ecf341ed05805d4e12aedf1b08d52570e
A Mon <a.mont.24.777@gmail.com>
2023-05-24 16:35:48 -0600

    Update Tutorial

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,14 @@
 import { useState } from "react";
-import ReactDOM from "react-dom";
 import {
-  ChakraProvider,
   Heading,
-  useToast,
   Box,
   Button,
   HStack,
   Spacer,
   Image,
 } from "@chakra-ui/react";
-import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
-import { GrUpdate } from "react-icons/gr";
 import Identicon from "@polkadot/react-identicon";
-import { App } from "../../App";
+import { AccountButton } from "./AccountModal";
 import "./app.css";
 
 function Header() {
@@ -23,40 +18,7 @@ function Header() {
 
   const publicKey = localStorage.getItem("Publickey");
 
-  const [Account, setAccount] = useState(publicKey);
-
-  const fistsegment = publicKey!.slice(0, 4);
-
-  const secondsegment = publicKey!.slice(-4);
-
-  const toast = useToast();
-  const [text, setText] = useState("");
-  const [copied, setCopied] = useState(false);
-
-  const handleCopy = () => {
-    setCopied(true);
-
-    const content = document.querySelector<HTMLInputElement>("#input")?.value;
-
-    if (content) {
-      navigator.clipboard.writeText(content);
-    }
-  };
-
-  const extensionaccount = async function () {
-    await web3Enable("My wallet");
-    const allAccounts = await web3Accounts();
-    const account = allAccounts[0].address;
-    localStorage.setItem("Publickey", account);
-    setAccount(localStorage.getItem("Publickey"));
-
-    ReactDOM.render(
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>,
-      document.getElementById("root")
-    );
-  };
+  const [Account] = useState(publicKey);
 
   return (
     <>
@@ -79,17 +41,9 @@ function Header() {
         <Spacer />
         <Box></Box>
         <Box>
-          <Button
-            variant="solid"
-            colorScheme="pink"
-            borderRadius="30px"
-            onClick={extensionaccount}
-          >
-            <GrUpdate color="white" size="20px" />
-          </Button>
           <Button backgroundColor="pink.500" borderRadius="20px">
             <Identicon value={Account} size={size} theme={theme} />
-            {fistsegment + "..." + secondsegment}
+            <AccountButton/> 
           </Button>
         </Box>
       </HStack>
